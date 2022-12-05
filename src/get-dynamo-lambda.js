aab96f9619f7a13be1bcbe5957ce66ff9d33e353
Aditya Mallapragada <aditya.mallapragada@infinitive.com>
2022-12-05 18:51:37 -0500

    Change get-dynamo-lambda to return error reponse immediately after validating data

--- src/get-dynamo-lambda.js
+++ src/get-dynamo-lambda.js
@@ -4,17 +4,27 @@ exports.handler = async function (event) {
   const dynamoClient = new DynamoDBClient({region: 'us-east-1'});
   const dynamoDocumentClient = DynamoDBDocumentClient.from(dynamoClient);
   const dynamoDBName = process.env.DYNAMO_TABLE_NAME;
-  const converted_uid = Number(event['queryStringParameters']['user_id']);
-  const user_id = Number.isNaN(converted_uid) ? -1 : converted_uid;
+  const error_response = {
+    "statusCode": 400,
+    "body": JSON.stringify({ 'body': 'ERROR: invalid request'}),
+    "isBase64Encoded": false,
+    "headers": {
+      'Content-Type': 'application/json'
+    }
+  };
+  const u_id = Number(event['queryStringParameters']['user_id']);
+  if (Number.isNaN(u_id)) {
+    return error_response;
+  } 
   const params = {
     TableName: dynamoDBName,
     Key: {
-      user_id: user_id
+      user_id: u_id
     }
   };
   const response = await dynamoDocumentClient.send(new GetCommand(params));
   if (response['Item'] !== undefined) {
-    const ret_response = {
+    return {
       "statusCode": response['$metadata']['httpStatusCode'],
       "body": JSON.stringify(response['Item']),
       "isBase64Encoded": false,
@@ -22,16 +32,7 @@ exports.handler = async function (event) {
         'Content-Type': 'application/json'
       }
     };
-    return ret_response;
 } else {
-    const ret_error = {
-      "statusCode": 400,
-      "body": JSON.stringify({ 'body': 'ERROR: invalid request'}),
-      "isBase64Encoded": false,
-      "headers": {
-        'Content-Type': 'application/json'
-      }
-    };
-    return ret_error;
+    return error_response;
   }
-}
+};
